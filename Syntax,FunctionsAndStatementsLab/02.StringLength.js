8a28391897d2e47b9d7bebdc82f801c6bfc164a5
Stela Kaneva <stela.marinova.eneva@gmail.com>
2021-09-15 16:56:31 +0300

    Add new folder with solutions of problems on Syntax, Functions and Statements topic

---
+++ Syntax,FunctionsAndStatementsLab/02.StringLength.js
@@ -0,0 +1,9 @@
+function stringLength(string1, string2, string3){
+    let totalLength = string1.length + string2.length + string3.length;
+    let averageLength = Math.floor(totalLength / 3);
+
+    console.log(totalLength);
+    console.log(averageLength);
+}
+
+stringLength('chocolate', 'ice cream', 'cake');
