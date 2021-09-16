ca34653d45b7dc5161934c748d14c715af9c190a
Stela Kaneva <stela.marinova.eneva@gmail.com>
2021-09-16 16:26:03 +0300

    Add new folder with solutions of problems on Syntax, Functions and Statements topic.

---
+++ Syntax,FunctionsAndStatements/Exercise/01.Fruit.js
@@ -0,0 +1,9 @@
+function calculate(fruit, weight, price){
+    let sum;
+    let weightInKg = weight / 1000;
+    sum = weightInKg * Number(price);
+
+    console.log(`I need $${sum.toFixed(2)} to buy ${weightInKg.toFixed(2)} kilograms ${fruit}.`);
+}
+
+calculate('apple', 1563, 2.35);
