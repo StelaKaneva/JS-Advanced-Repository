eadf2d2fe8d016bff234886440a1a1e5315bebd0
Stela Kaneva <stela.marinova.eneva@gmail.com>
2021-10-12 10:44:56 +0300

    Add solutions of problems on Advanced Functions topic

---
+++ AdvancedFunctions/Lab/05.CommandProcessor.js
@@ -0,0 +1,34 @@
+function solution() {
+    let text = '';
+
+    function append(string) {
+        text += string;
+    }
+
+    function removeStart(n) {
+        text = text.slice(n);
+    }
+
+    function removeEnd(n) {
+        text = text.slice(0, -n);
+    }
+
+    function print() {
+        console.log(text);
+    }
+
+    return {
+        append,
+        removeStart,
+        removeEnd,
+        print
+    };
+}
+
+let firstZeroTest = solution();
+
+firstZeroTest.append('hello');
+firstZeroTest.append('again');
+firstZeroTest.removeStart(3);
+firstZeroTest.removeEnd(4);
+firstZeroTest.print();
